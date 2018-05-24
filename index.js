0505e36fd2993b4d4a4cccaa497740130284b84e
ivan <ivan@eriad.com>
2018-05-24 21:44:18 +0800

    yah.

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const _       = require('lodash');
+const moment  = require('moment');
+const dataset = require('./data.json');
+
+
+const MyHolidays = function(year) {
+
+  const index = year || "2018";
+
+  this.holidays = dataset[index];
+  this.holidays.forEach( holiday => { holiday.date = moment(holiday.date).toDate(); });
+
+};
+
+MyHolidays.prototype.check = function (date) {
+
+  const mh = this;
+  const checkDate = moment(date).toDate();
+
+  return _.find(mh.holidays, {date:checkDate});
+};
+
+MyHolidays.prototype.list = function () {
+  return _.clone(this.holidays);
+};
+
+
+module.exports = MyHolidays;
